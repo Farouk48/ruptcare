0a5779caa2d898cf17e0f33aec9a9f2f9c70606f
victor bergeaud <victor.bergeaud@gmail.com>
2020-08-25 11:40:43 +0200

    Routes, controller et model de patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,9 @@
 class PatientsController < ApplicationController
+  def index
+    @patients = current_user.patients
+  end
+
+  def show
+    @patient = Patient.new
+  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,6 @@
 class Patient < ApplicationRecord
   has_many :patient_drugs
   belongs_to :doctor
-  belongs_to  :pharma
+  belongs_to :pharma
+  belongs_to :user, through: :pharma
 end
