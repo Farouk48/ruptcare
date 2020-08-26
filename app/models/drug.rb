21ac4d84875a026fefdd5e0c1dea619f2d33a5b9
victor bergeaud <victor.bergeaud@gmail.com>
2020-08-26 17:28:23 +0200

    implemented the pg search for models patient and drugs

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,4 +1,15 @@
 class Drug < ApplicationRecord
   has_many :stocks
   has_many :patient_drugs
+
+# include PgSearch::Model
+
+# pg_search_scope :search_by_name_and_actions
+  
+#   against: [:name, :actions],
+#   using: {
+#   	tsearch: {prefix: true}
+#   }
+
+
 end
