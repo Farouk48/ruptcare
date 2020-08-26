721c42e95f275226c3014c7dc9ff5079b3d3291b
victor bergeaud <victor.bergeaud@gmail.com>
2020-08-26 18:41:06 +0200

    set up the seed

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -2,14 +2,12 @@ class Drug < ApplicationRecord
   has_many :stocks
   has_many :patient_drugs
 
-# include PgSearch::Model
+  include PgSearch::Model
 
-# pg_search_scope :search_by_name_and_actions
-  
-#   against: [:name, :actions],
-#   using: {
-#   	tsearch: {prefix: true}
-#   }
-
+  pg_search_scope :search_by_name_and_action,
+	  against: [:name, :action],
+	  using: {
+	  	tsearch: {prefix: true}
+	  }
 
 end
