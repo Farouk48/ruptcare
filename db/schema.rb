5d858518f7c30d17fd7c28cc98b922ad0887a8a2
Farouk RAHMANI <rahmani.farouk@icloud.com>
2020-08-26 17:46:49 +0200

    fixing bug on migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_151930) do
+ActiveRecord::Schema.define(version: 2020_08_26_153043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_08_26_151930) do
     t.string "galenic"
     t.string "posology"
     t.string "recommandations"
-    t.integer "availibity"
+    t.integer "availability"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "administration"
