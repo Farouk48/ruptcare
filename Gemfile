cf945456a916b0a09adf62934efb5ffc318fab59
victor bergeaud <victor.bergeaud@gmail.com>
2020-08-26 16:12:54 +0200

    set up pg search for drugs

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'simple_form'
 
 gem 'geocoder'
 
+gem 'pg_search', '~> 2.3.0'
+
 
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,9 +1,13 @@
 class DrugsController < ApplicationController
 
   def index
+  	if params[:query].present?
+  		@drugs = Drug.search_by_name_and_actions(params[:query])
+  	else
     @drugs = Drug.all
   end
-
+  end
+  
   def show
     @drug = Drug.find(params[:id])
   end
