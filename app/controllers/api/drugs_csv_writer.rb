f3c478b86978ecd6386fde0670ba53aaae9843c0
Victor Bolta <victor.bolta@icloud.com>
2020-08-28 11:40:13 +0200

    Heroku fix

--- app/controllers/api/drugs_csv_writer.rb
+++
@@ -1,9 +0,0 @@
-require 'csv'
-
-csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
-filepath    = 'drugs.csv'
-
-CSV.open(filepath, 'wb', csv_options) do |csv|
-  csv << ['Name', '', '']
-
-end
