007feb323c5d14e35c608eaa15ea9a9c10ea07b0
Walid Tairi <tairiwalide@hotmail.fr>
2020-08-28 16:04:42 +0200

    buttons-modal5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,3 +37,17 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   initMapbox();
 });
+    document.addEventListener('turbolinks:before-cache', () => {
+      // Manually tear down bootstrap modals before caching. If turbolinks
+      // caches the modal then tries to restore it, it breaks bootstrap's JS.
+      // We can't just use bootstrap's `modal('close')` method because it is async.
+      // Turbolinks will cache the page before it finishes running.
+      if (document.body.classList.contains('modal-open')) {
+        $('.modal')
+          .hide()
+          .removeAttr('aria-modal')
+          .attr('aria-hidden', 'true');
+        $('.modal-backdrop').remove();
+        $('body').removeClass('modal-open');
+      }
+    });
