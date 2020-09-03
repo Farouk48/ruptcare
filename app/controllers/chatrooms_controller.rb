521e93f627e64cac3528358c1de57223403d97fc
victor bergeaud <victor.bergeaud@gmail.com>
2020-09-03 18:07:52 +0200

    chatroom layout

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,6 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
-    @patient = Patient.find(params[:patient_id])
     @message = Message.new
     authorize @chatroom
 
